537e9650fc44240afd487a1d309275aaf444312c
Gayeon Park <gypark@itsmart.co.kr>
2025-05-26 10:25:47 +0900

    firtst commit
    first commit

---
+++ src/main/java/kr/co/itsm/itsmart_profile_mnt2/ItsmartProfileMnt2Application.java
@@ -0,0 +1,13 @@
+package kr.co.itsm.itsmart_profile_mnt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsmartProfileMnt2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItsmartProfileMnt2Application.class, args);
+    }
+
+}
